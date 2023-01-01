3267de596bf5a58ab67c74faa63577bb042ba90a
Arifpro1 <johndoe@example.com[200~johndoe@example.com>
2023-01-02 01:22:10 +0600

    change in Wp function file

--- 80_WP_function_making.py
+++ 80_WP_function_making.py
@@ -1,4 +1,5 @@
-import wp_func
+from wp_func import wp_p
+from wp_func import h2
 
 first_p_text = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum"
 
@@ -6,5 +7,5 @@ heading_one_text = "Why do we use it ?"
 
 second_p_text = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
 
-article = wp_func.wp_p(first_p_text)+wp_func.h2(heading_one_text)+wp_func.wp_p(second_p_text)
+article = wp_p(first_p_text)+h2(heading_one_text)+wp_p(second_p_text)
 print(article)
